6f6b1afbcc6466e3b6a614ecada815e3ea17aad5
DESKTOP-62BLBMM\song_ <song_jun0@naver.com>
2023-08-03 18:03:00 +0900

    j

--- src/main/java/com/mni/userInfo/userInfoDAO.java
+++ src/main/java/com/mni/userInfo/userInfoDAO.java
@@ -146,12 +146,7 @@ public class userInfoDAO {
             if(rs.next()) {
                user_id = rs.getString(1);
             }
-<<<<<<< HEAD
-=======
-
             System.out.println(user_id);
-
->>>>>>> branch 'master' of https://github.com/lbr410/mni.git
             return user_id;
          }catch(Exception e) {
             e.printStackTrace();
